f02b9024d13997cd5f9f6a0756294b55cacb575e
Ryan S <ryansouza@usp.br>
2023-04-19 19:59:37 -0300

    style: sql formatter

--- create/create_tables.sql
+++ create/create_tables.sql
@@ -1,22 +1,23 @@
-/* CREATE TABLE */
-CREATE TABLE airports(
-id DOUBLE,
-ident VARCHAR(100),
-type VARCHAR(100),
-name VARCHAR(100),
-latitude_deg DOUBLE,
-longitude_deg DOUBLE,
-elevation_ft DOUBLE,
-continent VARCHAR(100),
-iso_country VARCHAR(100),
-iso_region VARCHAR(100),
-municipality VARCHAR(100),
-scheduled_service VARCHAR(100),
-gps_code VARCHAR(100),
-iata_code VARCHAR(100),
-local_code VARCHAR(100),
-home_link VARCHAR(100),
-wikipedia_link VARCHAR(100),
-keywords VARCHAR(100)
-);
+CREATE TABLE airports (
+    id DOUBLE,
+    ident VARCHAR(100),
+    type VARCHAR(100),
+    name VARCHAR(100),
+    latitude_deg DOUBLE,
+    longitude_deg DOUBLE,
+    elevation_ft DOUBLE,
+    continent VARCHAR(100),
+    iso_country VARCHAR(100),
+    iso_region VARCHAR(100),
+    municipality VARCHAR(100),
+    scheduled_service VARCHAR(100),
+    gps_code VARCHAR(100),
+    iata_code VARCHAR(100),
+    local_code VARCHAR(100),
+    home_link VARCHAR(100),
+    wikipedia_link VARCHAR(100),
+    keywords VARCHAR(100)
 
+    -- TODO: Adicionar chaves estrangeiras
+    -- TODO: Adicionar restrições
+);
