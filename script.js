0c1e48618abb5579a2f777af882d11d4adab5066
Ryan S <ryansouza@usp.br>
2023-04-22 15:06:37 -0300

    refactor: some changes to fit better with cleanCode

--- script.js
+++ script.js
@@ -2,19 +2,8 @@ const fs = require('fs');
 
 const nullValue = '\\N';
 
-// Read the file
-const csvFilename = process.argv[2];
-const file = fs.readFileSync(csvFilename, 'utf8');
-
-const isNullOrEmpty = (str) => str === nullValue || str === '';
-
-sqlInserts.forEach(sql => {
-    fs.appendFileSync('./output.sql', sql + '\n');
-})
-
-// TODO: Tratar espaços vazios em Values
-// TODO: Tratar Datas de inserção
-// TODO: Tratar virgula no final da linha
+const csvDirectory = './database';
+const insertsDirectory = './inserts';
 
 /**
  * Read a CSV file and return an array of objects
@@ -22,29 +11,39 @@ sqlInserts.forEach(sql => {
  * @param {*} file 
  * @returns object[]
  */
-function readCSV(file) {
-    const lines = file.split("\n");
-    const headers = lines[0].split(",").map(header => header.replace(/"/g, ''));
-    const csvParsed = [];
+const readCSV = (file) => {
+    const [headerLine, ...contentLines] = file.split('\n');
+    const headers = parseHeaders(headerLine);
+    const csvParsed = contentLines.map((line) => parseLine(line, headers));
 
-    lines.shift(); // Remove headers
-    for (const line of lines) {
-        const currentLine = line.split(",").map(value => value.replace(/"/g, ''));
-        if (currentLine.length > headers.length){
-            //There is at least one value with comma
-            currentLine[headers.length - 1] = currentLine.slice(headers.length - 1).join(',');
+    return [csvParsed, headers];
+
+    function parseHeaders(headerLine) {
+        return headerLine.split(',').map((header) => header.replace(/"/g, ''));
+    };
+
+    function parseLine(line, headers) {
+        const values = line.split(',').map((value) => value.replace(/"/g, ''));
+        const hasExtraValues = values.length > headers.length;
+
+        if (hasExtraValues) {
+            const remainingValues = values.slice(headers.length - 1);
+            const lastValue = remainingValues.join(',');
+            values[headers.length - 1] = lastValue;
         }
+
         const obj = {};
-    
-        for (const [index, header] of headers.entries()) {
-            obj[header] = isNullOrEmpty(currentLine[index]) ? null : currentLine[index];
-        }
-        
-        csvParsed.push(obj);
-    }
+        headers.forEach((header, index) => {
+            obj[header] = isNullOrEmpty(values[index]) ? 'null' : values[index];
+        });
 
-    return csvParsed;
-}
+        return obj;
+    };
+
+    function isNullOrEmpty(value) {
+        return value === nullValue || value === '';
+    };
+};
 
 /**
  * Convert Object to SQL Insert Statement
@@ -53,18 +52,58 @@ function readCSV(file) {
  * @param {*} headers 
  * @returns string[]
  */
-function convertToSQLInsertStatement(object, headers) {
-    const sql = [];
-    const csvFilename = process.argv[2].split('/')[1].split('.')[0];
-    for(const row of object) {
-        for(const header of headers) {
-            if (isNaN(row[header])) {
-                row[header] = `"${row[header]}"`;
-            }else{
-                row[header] = `${row[header]}`;
-            }
+const convertToSQLInsertStatement = (object, headers, databaseName) => {
+    return object.map((row) => {
+        const values = headers.map((header) => quoteIfNeeded(row[header]));
+        const sql = `INSERT INTO ${databaseName} (${headers.join(',')}) VALUES (${values.join(',')})`;
+        return sql;
+    });
+
+    function quoteIfNeeded(value) {
+        if (isNaN(value) && value !== 'null') {
+            return `"${value}"`;
         }
-        sql.push(`INSERT INTO ${csvFilename} (${headers.join(',')}) VALUES (${Object.values(row).join(",")})`);
-    }
-    return sql;
-}
+        return value;
+    };
+};
+
+const main = () => {
+    const csvFiles = getCsvFiles(csvDirectory);
+    createInsertsDirectory(insertsDirectory);
+
+    csvFiles.forEach((csvFile) => {
+        const databaseName = getDatabaseName(csvFile);
+        console.log(`Reading ${databaseName}...`);
+        const csvContent = fs.readFileSync(`${csvDirectory}/${csvFile}`, 'utf8');
+        const [csvParsed, headers] = readCSV(csvContent);
+        const sqlInserts = convertToSQLInsertStatement(csvParsed, headers, databaseName);
+        console.log(`Writing ${databaseName} inserts...`);
+        writeSqlInserts(sqlInserts, databaseName, insertsDirectory);
+    });
+
+    function getCsvFiles(directory) {
+        const files = fs.readdirSync(directory);
+        return files.filter((file) => file.endsWith('.csv'));
+    };
+
+    function createInsertsDirectory(directory) {
+        if (!fs.existsSync(directory)) {
+            fs.mkdirSync(directory);
+        }
+    };
+
+    function getDatabaseName(csvFile) {
+        return csvFile.split('.')[0];
+    };
+
+    function writeSqlInserts(sqlInserts, databaseName, directory) {
+        const fileName = `${databaseName}_inserts.sql`.toUpperCase();
+        const filePath = `${directory}/${fileName}`;
+        fs.writeFileSync(filePath, '');
+        sqlInserts.forEach((sql) => {
+            fs.appendFileSync(filePath, sql + '\n');
+        });
+    };
+};
+
+main();
