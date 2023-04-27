189a4a7d39badbb45bca19eda330e70ba2d8958d
João Lucas Pereira <j.lucaspereira@usp.br>
2023-04-26 21:38:20 -0300

    finished exercises 1 at 4

--- queries/queries.sql
+++ queries/queries.sql
@@ -6,3 +6,34 @@ FROM DRIVERSTANDINGS
          JOIN DRIVER ON DRIVERSTANDINGS.driverId = DRIVER.driverid
 GROUP BY RACES.year, DRIVER.driverid
 ORDER BY DRIVER.driverid ASC, total_points DESC;
+
+--================================================================================--
+
+-- EXERCÍCIO 2
+
+SELECT count(RACES.raceid) as count, RACES.year
+FROM RACES
+GROUP BY RACES.year
+ORDER BY count DESC;
+
+--================================================================================--
+
+-- EXERCÍCIO 3
+
+SELECT AIRPORTS.type, AIRPORTS.continent, count(AIRPORTS.ident)
+FROM AIRPORTS
+GROUP BY AIRPORTS.type, AIRPORTS.continent
+ORDER BY AIRPORTS.type ASC;
+
+--================================================================================--
+
+-- EXERCÍCIO 4
+
+ALTER TABLE QUALIFYING
+ADD Pole_Position VARCHAR(6);
+
+UPDATE QUALIFYING SET pole_position = 'Podium' WHERE position BETWEEN 1 AND 3;
+
+--================================================================================--
+
+-- EXERCÍCIO 5
