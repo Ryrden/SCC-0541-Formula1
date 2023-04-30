94c5cd5f4e2f40b21c4a80fc436512f9e20f5c25
João Lucas Pereira <j.lucaspereira@usp.br>
2023-04-30 14:43:31 -0300

    completed all exercises

--- queries/queries.sql
+++ queries/queries.sql
@@ -30,16 +30,82 @@ ORDER BY AIRPORTS.type ASC;
 -- EXERCÍCIO 4
 
 ALTER TABLE QUALIFYING
-ADD Pole_Position VARCHAR(6);
+    ADD Pole_Position VARCHAR(6);
 
-UPDATE QUALIFYING SET pole_position = 'Podium' WHERE position BETWEEN 1 AND 3;
+UPDATE QUALIFYING
+SET pole_position = 'Podium'
+WHERE position BETWEEN 1 AND 3;
 
 --================================================================================--
 
 -- EXERCÍCIO 5
 
-UPDATE DRIVER SET nationality = 'BR' WHERE nationality = 'Brazilian';
+UPDATE DRIVER
+SET nationality = 'BR'
+WHERE nationality = 'Brazilian';
 
 --================================================================================--
 
 -- EXERCÍCIO 6
+
+SELECT DRIVER.forename, DRIVER.surname, COUNT(*) AS pole_position
+FROM QUALIFYING
+         JOIN DRIVER ON QUALIFYING.driverid = DRIVER.driverid
+WHERE QUALIFYING.position = 1
+GROUP BY DRIVER.forename, DRIVER.surname, QUALIFYING.driverid
+ORDER BY pole_position DESC
+LIMIT 1;
+
+--================================================================================--
+
+-- EXERCÍCIO 7
+
+SELECT CITIES.country, num_cities, num_airports
+FROM (SELECT GEOCITIES15K.country, COUNT(DISTINCT GEOCITIES15K.geonameid) AS num_cities
+      FROM GEOCITIES15K
+               JOIN COUNTRIES ON GEOCITIES15K.country = COUNTRIES.code
+               JOIN CIRCUITS ON COUNTRIES.name = CIRCUITS.country
+      GROUP BY GEOCITIES15K.country) AS CITIES
+         JOIN (SELECT AIRPORTS.ISOCountry, COUNT(DISTINCT AIRPORTS.ident) AS num_airports
+               FROM AIRPORTS
+               GROUP BY AIRPORTS.ISOCountry) AS airports
+              ON AIRPORTS.ISOCountry = CITIES.country;
+
+--================================================================================--
+
+-- EXERCÍCIO 8
+
+-- Alternativa 1: Criar uma tabela com a mesma estrutura da tabela COUNTRIES, porém que contém apenas os países que tenham menos que 10 aeroportos, utilizando a claúsula NOT IN.
+-- Essa alternativa é a melhor em termos de complexidade, pois reduz a necessidade de criarmos uma tabela temporarária para armazenarmos os dados e depois deletarmos as tuplas que não atendem a condição especificada.
+CREATE TABLE countriesv2 AS
+SELECT *
+FROM COUNTRIES
+WHERE COUNTRIES.code NOT IN (SELECT AIRPORTS.isocountry
+                             FROM AIRPORTS
+                                      JOIN COUNTRIES ON AIRPORTS.isocountry = COUNTRIES.code
+                             GROUP BY AIRPORTS.isocountry, COUNTRIES.name
+                             HAVING COUNT(AIRPORTS.ident) > 10
+                             ORDER BY AIRPORTS.isocountry);
+
+-- Alternativa 2: Essa alternativa também contempla a criação de uma tabela COUNTRIESV2 com a mesma estrutura da tabela COUNTRIES, porém que contém apenas os países que tenham menos que 10 aeroportos.
+-- Nessa alternativa, utilizamos a claúsula NOT IN e a claúsula DELETE, como especificado no exercício. Entretanto, vale ressaltar que,  comando DELETE não é necessário para essa operação. Em vez disso, seria possível utilizar uma cláusula NOT IN para excluir os países que têm mais de 10 aeroportos. Ou seja, uma clausúla substitui a outra.
+-- Além disso, tal alternativa aumenta a complexidade de operação, visto que é necessário criar uma tabela temporária desnecessariamente apenas para fazer tal operação.
+-- Dito isso, foi necessário seguir os seguintes passos:
+-- 1. Criamos uma tabela TEMPORÁRIA que contém TODOS os países (é uma cópia da tabela COUNTRIES).
+-- 2. Deletamos todos países que tem MENOS de 10 aeroportos, da tabela TEMPORÁRIA.
+-- 3. Criamos a tabela  COUNTRIESV2 que seleciona todos os países da tabela COUNTRIES e utiliza a cláusula NOT IN para selecionar APENAS aqueles países que estão na tabela COUNTRIES mas não estão na tabela TEMPORÁRIA, ou seja, apenas aqueles países que tem <= 10 aeroportos.
+
+CREATE TEMPORARY TABLE temp AS
+SELECT AIRPORTS.ISOCountry, COUNT(AIRPORTS.ident) as count
+FROM AIRPORTS
+GROUP BY AIRPORTS.ISOCountry;
+
+DELETE
+FROM TEMP
+WHERE TEMP.count <= 10;
+
+CREATE TABLE COUNTRIESv2 AS
+SELECT *
+FROM COUNTRIES
+WHERE COUNTRIES.code NOT IN (SELECT TEMP.ISOCountry FROM TEMP);
+
